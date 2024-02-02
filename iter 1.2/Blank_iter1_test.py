a98c18484fe47493419505894fcf995074db7f22
Paul Kallarackel <paulkallarackel@gmail.com>
2024-02-02 12:06:11 +0000

    fixe buckle script

--- iter 1.2/Blank_iter1_test.py
+++ iter 1.2/Blank_iter1_test.py
@@ -236,3 +236,43 @@ session.writeFieldReport(fileName=os.path.join(texts_directory,'Stresses everywh
     sortItem='Node Label', odb=odb, step=0, frame=1, outputPosition=NODAL, 
     variable=(('S', INTEGRATION_POINT, ((INVARIANT, 'Mises'), )), ), 
     stepFrame=SPECIFY)
+
+
+# Buckling
+mdb.Model(name='Model-1-Buckle', objectToCopy=mdb.models['Model-1'])
+mdb.models['Model-1-Buckle'].steps.changeKey(fromName='Loading down with g', 
+    toName='buckling')
+mdb.models['Model-1-Buckle'].steps['buckling'].setValues(description=
+    'Loading with buckling')
+mdb.models['Model-1-Buckle'].BuckleStep(blockSize=DEFAULT, description='buckling'
+    , eigensolver=LANCZOS, maintainAttributes=True, maxBlocks=DEFAULT, 
+    minEigen=0.0, name='buckling', numEigen=1, previous='Initial')
+del mdb.models['Model-1-Buckle'].loads['Gravity']
+mdb.models['Model-1-Buckle'].loads['Loading centre'].setValues(cf2=-1.0, 
+    distributionType=UNIFORM, field='')
+
+myJob2 = mdb.Job(name='Job-1-Buckle', model='Model-1-Buckle', description='jobDescription')
+myJob2.submit()
+myJob2.waitForCompletion()
+# Get the current working directory
+current_directory = os.getcwd()
+
+# Define the relative path to the ODB file
+relative_path = 'Job-1-Buckle.odb'
+
+# Join the current working directory with the relative path
+absolute_path = os.path.join(current_directory, relative_path)
+
+r = session.openOdb(name=absolute_path)
+Frames = r.steps["buckling"].frames
+BuckleLoad = []
+for i in range (1,len(Frames)):
+    EigenV = Frames[i].description
+    list = EigenV.split()
+    Load = float(list[-1])
+    BuckleLoad = BuckleLoad + [Load]
+
+# Buckle Load
+# Display the Buckle Load properties
+with open(os.path.join(texts_directory,'Buckle Load.txt'), 'w') as file:
+    file.write("Buckle Load: {}\n".format( BuckleLoad[0]))
